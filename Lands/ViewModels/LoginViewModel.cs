41f03ecb16c4a3c93c7ecd7bca68164a971a4830
sergio Garcia <sgarcia@grupoproval.com>
2018-02-13 15:04:54 -0600

    login view model

---
+++ Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,49 @@
+﻿namespace Lands.ViewModels
+{
+    using System;
+    using System.Windows.Input;
+
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get;
+            set;
+        }
+        #endregion
+    }
+}
--- Lands/ViewModels/MainViewModel.cs
+++ Lands/ViewModels/MainViewModel.cs
@@ -1,9 +1,29 @@
 ﻿using System;
 // Esta es la clase principal y la que gobierna a todas las del proyecto
+using System.Diagnostics.Contracts;
+using Xamarin.Forms;
 namespace Lands.ViewModels
 {
     public class MainViewModel
     {
+        #region ViewModels
+        // se referncia la Login
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        // por ser la pagina principal debemos instanciarla
+        // ctor tab+tab contruye en constructor
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+            // al poner this decimos que es una propiedad de la clase es obligatorio
+        }
+        #endregion
 
     }
 }
