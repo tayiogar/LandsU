789c05ff4a469124a455071fb4154e0b63ce6646
sergio Garcia <sgarcia@grupoproval.com>
2018-03-05 13:55:02 -0600

    land backen

---
+++ Lands.API/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Http;
+
+namespace Lands.API
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
---
+++ Lands.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Lands.Domain
+{
+    public class Class1
+    {
+    }
+}
